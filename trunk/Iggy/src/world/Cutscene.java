e84d40dccddf8c2ab304df7c37cdf019ba5cafb5
Yestin.Harrison@gmail.com <Yestin.Harrison@gmail.com@28993f55-a130-0686-b18d-9bc86256617c>
2012-12-30 19:42:13 +0000

    FAT COCKS

--- trunk/Iggy/src/world/Cutscene.java
+++ trunk/Iggy/src/world/Cutscene.java
@@ -12,8 +12,6 @@ import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -24,6 +22,7 @@ public class Cutscene {
     boolean advance;
     Scanner reader;
     String currentText;
+    
     public Cutscene(int level){
         try {
             reader=new Scanner(new File("Cutscenes/Cutscene"+level+".txt"));
@@ -50,13 +49,13 @@ public class Cutscene {
     public void resetAdvance(){
         advance=true;
     }
-    public void draw(ImageCollection batch,ViewScreen vs,Dimension d){
-        Vector2 v=new Vector2(-vs.GetX()+10,-vs.GetY()+d.height-205);
-        int w=d.width-20;
-        int h=200;
-        batch.fillRoundRect(v, w, h, 10, 10, Color.lightGray, 200);
-        batch.drawRoundRect(v, w, h, 10, 10, Color.BLACK, 200);
+    public void draw(ImageCollection batch,ViewScreen vs,Dimension d,Level level){
+        Vector2 v=new Vector2(-vs.GetX()+100,-vs.GetY()+d.height-205);
+        int w=d.width-200;
+        int h=100;
+        batch.fillRoundRect(v, w, h, 10, 10, new Color(8, 46, 3), 200);
+        batch.drawRoundRect(v, w, h, 10, 10, level.getSide(), 200);
         v.add(new Vector2(25,25));
-        batch.DrawString(v, currentText, Color.black, 210);
+        batch.DrawString(v, currentText, Color.WHITE, 210);
     }
 }
